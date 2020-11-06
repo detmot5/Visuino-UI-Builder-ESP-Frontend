8109e5404be9ad6671988ef9a472d0de1b982aa7
Norbert Bielak <norbertbielak@gmail.com>
2020-11-06 23:19:33 +0100

    Some renderer improvements

--- index.js
+++ index.js
@@ -5,38 +5,21 @@ const wrapper = document.getElementById('wrapper');
 const content = document.getElementById('content');
 const title = document.getElementById('title');
 
-
-fetch('test.json')
-  .then(response => response.json())
-  .then(data => {
-    renderData(data)
-    components.forEach(el => {
-      content.appendChild(el.render());
-      console.log(el)
+const getData = async () => {
+  await fetch('test.json')
+    .then(response => response.json())
+    .then(data => {
+      renderData(data)
+      components.forEach(el => {
+        const element = document.getElementById(el.name);
+        if(element === null) content.appendChild(el.render());
+      })
     })
-  })
-
-
-
-const label = new Label({
-  name: "eluwina",
-  state: "Cos jest włączone",
-  fontSize: 15,
-  dataType: 'boolean',
-  color: 'lightblue',
-  posX: 321,
-  posY: 440,
-  height: 10,
-  width: 40,
-  desktopScale: 2,
-  componentType: 'label'
-})
-
-content.appendChild(label.render());
-
-const getData = () => {
-
+  setTimeout(getData, 500);
 }
+
+
+
 getData();
 
 
--- renderer.js
+++ renderer.js
@@ -1,52 +1,43 @@
 
-
-
-const renderComponent = (component) => {
-}
-
-
-
-
-
 const renderData = ({elements}) => {
   console.log("render")
 
   console.log(elements)
   elements.forEach((el) => {
     const existing = getElementIfIsRendered(el);
-    if(existing === null){
       switch (el.componentType){
         case 'switch':
-          components.push(new SwitchComponent({
-            name: el.name,
-            componentType: el.componentType,
-            size: el.size,
-            posX: el.posX,
-            posY: el.posY,
-            state: el.value,
-            color: el.color,
-            desktopScale: el.desktopScale,
-          }));
+          if(existing === null) {
+            components.push(new SwitchComponent({
+              name: el.name,
+              componentType: el.componentType,
+              size: el.size,
+              posX: el.posX,
+              posY: el.posY,
+              state: el.value,
+              color: el.color,
+              desktopScale: el.desktopScale,
+            }));
+          }
           break;
 
         case 'label':
-          components.push(new Label({
-            name: el.name,
-            componentType: el.componentType,
-            posX: el.posX,
-            posY: el.posY,
-            width: el.width,
-            height: el.height,
-            state: el.value,
-            desktopScale: el.desktopScale,
-            fontSize: el.fontSize,
-            color: el.color,
-          }));
+          if(existing === null) {
+            components.push(new Label({
+              name: el.name,
+              componentType: el.componentType,
+              posX: el.posX,
+              posY: el.posY,
+              width: el.width,
+              height: el.height,
+              state: el.value,
+              desktopScale: el.desktopScale,
+              fontSize: el.fontSize,
+              color: el.color,
+            }));
+          } else existing.setState(el.value);
           break;
-
       }
-    }
-
   });
 
 
