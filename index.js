92aec34587b833904e38ecfc42c5ebe7aa95940b
Norbert Bielak <norbertbielak@gmail.com>
2020-11-08 16:23:50 +0100

    Added Connection indicator

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 const url = window.location.href;
+const connectedStr = "Connected";
+const disconnectedStr = "Disconnected";
+
 
 const components = [];
 const wrapper = document.getElementById('wrapper');
 const content = document.getElementById('content');
 const title = document.getElementById('title');
+const isConnectedDiv = document.getElementById('isConnected');
+
+
+
+
 
 const getData = async () => {
-  await fetch('test.json')
+  const controller = new AbortController();
+  setTimeout(() => controller.abort(), 10000);
+  await fetch('test.json', {signal: controller.signal })
     .then(response => response.json())
     .then(data => {
       renderData(data)
       components.forEach(el => {
         const element = document.getElementById(el.name);
         if(element === null) content.appendChild(el.render());
+        isConnectedDiv.innerHTML = connectedStr;
+        isConnectedDiv.style.color = '#00a103';
       })
+    })
+    .catch(error => {
+      isConnectedDiv.innerHTML = disconnectedStr;
+      isConnectedDiv.style.color = '#a10000';
     })
   setTimeout(getData, 500);
 }
