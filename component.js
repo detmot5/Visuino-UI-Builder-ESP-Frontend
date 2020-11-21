885441c7c86686062fb4ae8a225463f121fef94c
Norbert Bielak <norbertbielak@gmail.com>
2020-11-21 23:29:01 +0100

    Second Final

--- component.js
+++ component.js
@@ -1,11 +1,10 @@
 const componentWrapperClassName = 'c-wrapper';
 class Component {
-  constructor({name, componentType, posX, posY, value, desktopScale}) {
+  constructor({name, componentType, posX, posY, value}) {
     this.name = name;
     this.componentType = componentType;
     this.posX = posX;
     this.posY = posY;
-    this.desktopScale = desktopScale;
     this.value = value;
     this.wrapper = document.createElement('div');
     this.dFragment = document.createDocumentFragment();
@@ -23,8 +22,8 @@ class Component {
 }
 
 class InputComponent extends Component {
-  constructor({name, componentType, posX, posY, value, desktopScale }) {
-    super({name, componentType, posX, posY, value, desktopScale});
+  constructor({name, componentType, posX, posY, value }) {
+    super({name, componentType, posX, posY, value});
   }
   toJson() {
     return{
@@ -52,16 +51,16 @@ class InputComponent extends Component {
 }
 
 class OutputComponent extends Component{
-  constructor({name, componentType, posX, posY, value, desktopScale }) {
-    super({name, componentType, posX, posY, value, desktopScale});
+  constructor({name, componentType, posX, posY, value}) {
+    super({name, componentType, posX, posY, value});
   }
 
 }
 
 
 class Label extends OutputComponent {
-  constructor({name, componentType, posX, posY, value, desktopScale, fontSize, color }) {
-    super({name, componentType, posX, posY, value, desktopScale});
+  constructor({name, componentType, posX, posY, value, fontSize, color }) {
+    super({name, componentType, posX, posY, value});
     if(typeof value !== 'string') throw `${this.toString()} Illegal Parameter Type`;
     this.fontSize = fontSize;
     this.color = color;
@@ -89,8 +88,8 @@ class Label extends OutputComponent {
 }
 
 class GaugeComponent extends OutputComponent{
-  constructor({name,componentType, posX, posY, value, desktopScale, color, minValue, maxValue, width, height}) {
-    super({name, componentType, posX, posY, value, desktopScale});
+  constructor({name,componentType, posX, posY, value, color, minValue, maxValue, width, height}) {
+    super({name, componentType, posX, posY, value});
 
     this.color = color;
     this.minValue = minValue;
@@ -125,45 +124,63 @@ class GaugeComponent extends OutputComponent{
     return this.dFragment;
   }
 }
+
+
 class LedIndicatorComponent extends OutputComponent{
-  constructor({name, componentType, posX, posY, value, desktopScale, color }) {
-    super({name, componentType, posX, posY, value, desktopScale });
-    this.ledClassName = "led";
-    this.frameClassName = "frame";
-    this.offColor = "#999";
+  constructor({name, componentType, posX, posY, value, color, size }) {
+    super({name, componentType, posX, posY, value});
     this.color = color;
-    this.frame = document.createElement('div');
-    this.led = document.createElement('div');
+    this.size = size;
+    this.inner = document.createElement('div');
+    this.outer = document.createElement('div');
+
+    this.innerClassName = "inner-led";
+    this.outerClassName = "outer-led";
+    this.outerToInnerSizeFactor = 5/9;
+    this.offColor = "#777";
+    this.outerColor = "#555";
+    this.innerSize = Math.round(this.size * this.outerToInnerSizeFactor);
   }
 
   setState(state) {
     super.setState(state);
-    this.setLed(state.value);
     this.color = state.color;
+    this.setLed(state.value);
   }
+
+
   setLed(value){
-    if(value) {
-      this.led.style.backgroundColor = "red";
+    if(value){
+      this.inner.style.backgroundColor = this.color;
+      this.inner.style.boxShadow = `0 0 ${Math.round(this.innerSize / 10)}px ${this.color}`
     } else {
-      this.led.style.backgroundColor = "red";
+      this.inner.style.backgroundColor = this.offColor;
+      this.inner.style.boxShadow = `0 0 ${Math.round(this.innerSize / 10)}px ${this.color}`;
     }
   }
+
+
   render() {
     super.render();
-    this.led.className = this.ledClassName;
-    this.frame.className = this.frameClassName;
+    this.outer.className = this.outerClassName;
+    this.inner.className = this.innerClassName;
+    this.outer.style.width = `${this.size}px`;
+    this.outer.style.height = `${this.size}px`;
+    this.outer.style.backgroundColor = this.outerColor;
+    this.inner.style.width = `${this.innerSize}px`;
+    this.inner.style.height = `${this.innerSize}px`;
     this.setLed(this.value);
 
-    this.wrapper.appendChild(this.led);
-    this.wrapper.appendChild(this.frame);
+    this.outer.appendChild(this.inner);
+    this.wrapper.appendChild(this.outer);
     this.dFragment.appendChild(this.wrapper);
     return this.dFragment;
   }
 }
 
 class SwitchComponent extends InputComponent {
-  constructor({name, componentType, posX, posY, value, desktopScale, color, size }) {
-    super({name, componentType, posX, posY, value, desktopScale });
+  constructor({name, componentType, posX, posY, value, color, size }) {
+    super({name, componentType, posX, posY, value});
     if(typeof value !== 'boolean') throw `Switch ${this.toString()} Illegal Value Type`;
     this.switchClassName = "onoffswitch";
     this.switchCheckBoxClassName = "onoffswitch-checkbox";
@@ -193,8 +210,8 @@ class SwitchComponent extends InputComponent {
 
 class SliderComponent extends InputComponent{
 
-  constructor({name, componentType, width, height, posX, posY, value, desktopScale, color, maxValue, minValue }) {
-    super({name, componentType, width, height, posX, posY, value, desktopScale});
+  constructor({name, componentType, width, height, posX, posY, value, color, maxValue, minValue }) {
+    super({name, componentType, width, height, posX, posY, value});
     this.sliderClassName = 'slider';
     this.sliderValueWrapperClassName = 'sliderValueWrapper';
     this.minValue = minValue;
@@ -271,17 +288,15 @@ class SliderComponent extends InputComponent{
 }
 
 class NumberInputComponent extends InputComponent {
-  constructor({name, componentType, posX, posY, value, desktopScale, fontSize, width, color}) {
-    super({name, componentType, posX, posY, value, desktopScale });
+  constructor({name, componentType, posX, posY, value, fontSize, width, color}) {
+    super({name, componentType, posX, posY, value});
     if(typeof value !== 'number') throw `Number Input ${this.toString()} Illegal Value Type`;
     this.inputClassName = 'input';
     this.submitButtonClassName = 'input-btn';
     this.width = width;
     this.fontSize = fontSize;
     this.color = color;
-
     this.input = document.createElement('input');
-    this.submitButton = document.createElement('button');
   }
 
 
@@ -295,22 +310,20 @@ class NumberInputComponent extends InputComponent {
   }
 
 
-  setButtonColor(color){
-    this.submitButton.style.border = `solid ${color}`;
-    this.submitButton.style.borderWidth = '0 3px 3px 0';
+  setColor(color){
+    this.input.style.borderBottom = `solid 2px ${color}`;
   }
 
   notifyAboutSendingStatus(color) {
-    this.setButtonColor(color);
+    this.setColor(color);
     setTimeout(() => {
-      this.setButtonColor('#333');
+      this.setColor(this.color);
     },500);
   }
 
   render() {
     super.render();
     this.input.className = this.inputClassName;
-    this.submitButton.className = this.submitButtonClassName;
 
     this.wrapper.style.display = "flex";
     this.wrapper.style.flexDirection = "row";
@@ -325,15 +338,10 @@ class NumberInputComponent extends InputComponent {
     this.input.style.borderBottom = `solid 1px ${this.color}`;
     this.input.addEventListener('focusout', (e) => {e.target.style.borderBottom = `solid 1px ${this.color}`});
     this.input.addEventListener('focus', (e) => {e.target.style.borderBottom = `solid 2px ${this.color}`});
-
+    this.input.addEventListener('change', (e) => {this.onSubmit(e)})
     this.input.placeholder = this.name;
-    this.submitButton.style.padding = `${ Math.round(this.fontSize / 5) }px`;
-    this.submitButton.addEventListener('click', (e) => {this.onSubmit(e)});
-
 
     this.wrapper.appendChild(this.input);
-    this.wrapper.appendChild(this.submitButton);
-
     this.dFragment.appendChild(this.wrapper);
     return this.dFragment;
   }
@@ -343,8 +351,8 @@ class NumberInputComponent extends InputComponent {
 class ButtonComponent extends InputComponent{
 
 
-  constructor({name, componentType, posX, posY, value, desktopScale, color, text, textColor, fontSize, width, height}) {
-    super({name, componentType, posX, posY, value, desktopScale});
+  constructor({name, componentType, posX, posY, value, color, text, textColor, fontSize, width, height}) {
+    super({name, componentType, posX, posY, value});
     this.buttonClassName = "button";
     this.isButtonPressed = false;
     this.color = color;
